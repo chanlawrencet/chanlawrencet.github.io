4cdb96246ad3f2d6000e805b3ae9759810cec330
Lawrence Chan <chanlawrencet@gmail.com>
2020-10-04 00:32:35 -0400

    500px link

--- src/components/Links.js
+++ src/components/Links.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Github from '../assets/github.png'
 import LinkedIn from '../assets/linkedin.png'
+import PhotoPx from '../assets/500px.png'
 const Links = () => (
   <div
     style={{
@@ -18,6 +19,11 @@ const Links = () => (
       style={{marginRight: 20}}
       href='https://www.linkedin.com/in/chanlawrencet/'>
       <img height={40} width={40} src={LinkedIn}/>
+    </a>
+    <a
+      style={{marginRight: 20}}
+      href='https://500px.com/p/chanlawrencet?view=photos'>
+      <img height={20} width={75} src={PhotoPx}/>
     </a>
     <a
       style={{fontSize: 17}}
