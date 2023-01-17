f8161b0ccf4a286e92c7a4418e2cad806519c6bc
Lawrence Chan <chanlawrencet@gmail.com>
2023-01-17 16:34:36 -0500

    x axis ticks and labels

--- src/weather/Chart.js
+++ src/weather/Chart.js
@@ -22,13 +22,16 @@ export const Chart = ({softYMax=undefined, series, title='', yLabel, showLegend=
         softMax: softYMax
       },
       xAxis: {
-        title: {
-          text: 'time'
-        },
         labels: {
           enabled: true,
           formatter: (v) => {
-            return dayjs.unix(v.value).format('ha')
+            const timeString =  dayjs.unix(v.value).format('ha')
+            if (timeString === '12pm') {
+              return 'noon';
+            } else if (timeString === '12am') {
+              return 'mdnt';
+            }
+            return timeString.replace('pm', '').replace('am', '');
           }
         },
         min: dayjs().startOf('day').add(day, 'day').unix(),
@@ -38,8 +41,9 @@ export const Chart = ({softYMax=undefined, series, title='', yLabel, showLegend=
           value: dayjs().unix() // Position, you'll have to translate this to the values on your x axis
         }],
         tickPositioner: function () {
-          const ticks = this.series[0].processedXData.slice();
+          const ticks = this.series[0].processedXData.slice().filter((x, i) => i % 2 === 0);
           ticks.info = this.tickPositions.info;
+          console.log({ticks})
           return ticks;
         }
       },
