07b4f1af99b8705ee5884a03a433e8e98b69e63f
Lawrence Chan <chanlawrencet@gmail.com>
2025-06-06 21:30:33 -0400

    title

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.97ec795bd2e801b2b11e64ff7e7019e0.js"
+  "/precache-manifest.e5d8e37eeef9291dbbe9c0debe8b86e0.js"
 );
 
 self.addEventListener('message', (event) => {
